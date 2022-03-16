d9ead1c856c0b968d90e1329794d61849e1e018d
Michael Milton <michael.r.milton@gmail.com>
2022-03-16 12:12:20 +1100

    Update amplify demo and tests to check this fix

--- amplify/src/graphql/queries.js
+++ amplify/src/graphql/queries.js
@@ -34,8 +34,46 @@ export const listFoos = /* GraphQL */ `
     }
   }
 `;
+export const getBar = /* GraphQL */ `
+  query GetBar($id: ID!) {
+    getBar(id: $id) {
+      id
+      string_field
+      int_field
+      float_field
+      bool_field
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listBars = /* GraphQL */ `
+  query ListBars(
+    $filter: ModelBarFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listBars(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        string_field
+        int_field
+        float_field
+        bool_field
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
 export const countFoo = /* GraphQL */ `
   query CountFoo($filter: ModelFooFilterInput) {
     countFoo(filter: $filter)
+  }
+`;
+export const countBar = /* GraphQL */ `
+  query CountBar($filter: ModelBarFilterInput) {
+    countBar(filter: $filter)
   }
 `;
--- amplify/src/graphql/subscriptions.js
+++ amplify/src/graphql/subscriptions.js
@@ -40,3 +40,42 @@ export const onDeleteFoo = /* GraphQL */ `
     }
   }
 `;
+export const onCreateBar = /* GraphQL */ `
+  subscription OnCreateBar {
+    onCreateBar {
+      id
+      string_field
+      int_field
+      float_field
+      bool_field
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateBar = /* GraphQL */ `
+  subscription OnUpdateBar {
+    onUpdateBar {
+      id
+      string_field
+      int_field
+      float_field
+      bool_field
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteBar = /* GraphQL */ `
+  subscription OnDeleteBar {
+    onDeleteBar {
+      id
+      string_field
+      int_field
+      float_field
+      bool_field
+      createdAt
+      updatedAt
+    }
+  }
+`;
